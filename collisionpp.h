30ed99cd9205938df34d87dd27eb7616c6755147
fedi hannachi <61322407+hannachifedi@users.noreply.github.com>
2020-04-15 19:58:42 +0200

    Update collisionpp.h

--- collisionpp.h
+++ collisionpp.h
@@ -1,6 +1,2 @@
 SDL_Color GetPixel (SDL_Surface *pSurface , int x , int y);
-void update_points_de_collision(perssonage* hero);
-int collision_player_map_droite(background* acteurs , perssonage* hero);
-int collision_player_map_gauche(background* acteurs , perssonage* hero);
-int collision_player_map_top(background* acteurs , perssonage* hero);
-int collision_player_map_bas(background* acteurs , perssonage* hero);
+int collision(SDL_Surface *masque,SDL_Rect position_joueur);
